55402bfb1d57ee372d5ef076d68cf9d39fdf0eca
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-02-06 19:38:25 +0100

    test

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen bg-red-100">
+        <Nav/>
+<h1 class="mt-10">sdsdsdsd</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
