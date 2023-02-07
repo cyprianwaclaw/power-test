9d085e12c5e8fcf2547dfbf3f747ebdef2f2a3de
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-02-07 12:38:52 +0100

    test v2

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-blue-100">
+  <div class="max-h-screen bg-blue-100">
     <div class="" @click="inputColor()">
       <h1 class="">Logowanie</h1>
       <Form
