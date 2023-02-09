09cb78cf5746c582591324ababa9fe855e4557f1
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-02-09 13:05:56 +0100

    my-account view step1

--- pages/panel/konto/index.vue
+++ pages/panel/konto/index.vue
@@ -2,12 +2,21 @@
     <NuxtLayout name="panel">
 <h1 class="title-h1">Twoje konto</h1>
 <div>
-  
+
+<div class="columns-2 flex  gap-5">
   <div class="retangle gold"><NuxtLink to="">
     <Icon name="ph:game-controller-fill" color="D19D38" size="35"/>
     <p class="retangle-title family">Quizy</p>
   </NuxtLink></div>
-
+  <div class="retangle gold"><NuxtLink to="">
+    <Icon name="ph:game-controller-fill" color="D19D38" size="35"/>
+    <p class="retangle-title family">Quizy</p>
+  </NuxtLink></div>
+</div>
+<div class="retangle gold"><NuxtLink to="">
+  <Icon name="ph:game-controller-fill" color="D19D38" size="35"/>
+  <p class="retangle-title family">Quizy</p>
+</NuxtLink></div>
 </div>
     </NuxtLayout>
   </template>
@@ -31,4 +40,25 @@
     await authStore.logout();
   }
   </script>
-  
+  <style scoped>
+  .retangle{
+padding: 21px 20px 21px 24px;
+margin-bottom: 20px;
+border-radius: 8px;
+width: 100%;
+  }
+  .retangle-title{
+font-size: 17px;
+font-weight: 500;
+margin-top: 5px;
+}
+  .green{
+    background: rgba(56, 209, 172, 0.17);
+  }
+  .gold{
+    background: rgba(209, 157, 56, 0.17);
+  }
+  .blue{
+    background: rgba(43, 59, 199, 0.17);
+  }
+  </style>
