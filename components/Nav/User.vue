3c5836c0ad4a17b9c8451ba744f6db64c0a9de36
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-02-10 15:56:49 +0100

    aa

--- components/Nav/User.vue
+++ components/Nav/User.vue
@@ -2,7 +2,7 @@
   <div class="nav-border py-4 px-6 sm:py-4 fixed w-full flex bg-white z-10">
     <div class="flex w-full justify-between">
     <NuxtLink to="/panel"><img src="@/assets/file/logo.png" class="size-logo" /></NuxtLink>
-<NuxtLink to="/panel/szukaj"><Icon name="ph:magnifying-glass-light" size="30" class="nav-icon"/></NuxtLink>
+<NuxtLink to="/panel/szukaj"><Icon name="ph:magnifying-glass-light" size="30" class="search-icon"/></NuxtLink>
     </div>
   </div>
 </template>
@@ -19,5 +19,7 @@
     width: 175px;
   }
 }
-
+.search-icon{
+  color: #C4C4C4;
+}
 </style>
