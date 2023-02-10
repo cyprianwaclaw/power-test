41c78f8fd7a9f708353ff175627779ae04fd7057
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-02-10 15:44:34 +0100

    test

--- components/Nav/User.vue
+++ components/Nav/User.vue
@@ -1,24 +1,23 @@
 <template>
-    <div class="nav-border py-4 px-6 sm:py-4 fixed w-full flex bg-white z-10">
-      <div class="flex w-full justify-between">
-        <div class=""><NuxtLink to="/panel"><img src="@/assets/file/logo.png" class="size-logo" /></NuxtLink></div>
- <div class=""> <NuxtLink to="/panel/szukaj"><Icon name="ph:magnifying-glass-light" size="30" class="nav-icon"/></NuxtLink></div>
-      </div>
+  <div class="nav-border py-4 px-6 sm:py-4 fixed w-full flex bg-white z-10">
+    <div class="flex w-full justify-between">
+      <div class=""><NuxtLink to="/panel"><img src="@/assets/file/logo.png" class="size-logo" /></NuxtLink></div>
+<div class=""> <NuxtLink to="/panel/szukaj"><Icon name="ph:magnifying-glass-light" size="30" class="nav-icon"/></NuxtLink></div>
     </div>
-  </template>
-  <script setup lang="ts"></script>
-  <style scoped>
-  @media only screen and (max-width: 520px) {
-    .size-logo {
-      width: 175px;
-    }
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+@media only screen and (max-width: 520px) {
+  .size-logo {
+    width: 175px;
   }
-  
-  @media only screen and (min-width: 520px) {
-    .size-logo {
-      width: 175px;
-    }
-  }
+}
 
-  </style>
-  
+@media only screen and (min-width: 520px) {
+  .size-logo {
+    width: 175px;
+  }
+}
+
+</style>
