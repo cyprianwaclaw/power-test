abeb4f5e1c602c528830b105f688482ee91abdb3
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-02-10 15:48:39 +0100

    test

--- components/Nav/User.vue
+++ components/Nav/User.vue
@@ -2,7 +2,7 @@
   <div class="nav-border py-4 px-6 sm:py-4 fixed w-full flex bg-white z-10">
     <div class="flex w-full justify-between">
       <div class=""><NuxtLink to="/panel"><img src="@/assets/file/logo.png" class="size-logo" /></NuxtLink></div>
-<div class=""> <NuxtLink to="/panel/szukaj"><Icon name="ph:magnifying-glass-light" size="30" class="nav-icon"/></NuxtLink></div>
+<!-- <div class=""> <NuxtLink to="/panel/szukaj"><Icon name="ph:magnifying-glass-light" size="30" class="nav-icon"/></NuxtLink></div> -->
     </div>
   </div>
 </template>
