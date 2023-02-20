3cce770ba0ead7b8c87af563de8e93171197cb10
Cyprian <cyprianwaclaw@gmail.com>
2023-02-20 17:19:19 +0100

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <!-- <Meta name="viewport" content="My app description" /> -->
+  <Meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
   <div class="family black-color page-background">
     <div class="click">
       <!-- <NuxtLoadingIndicator /> -->
