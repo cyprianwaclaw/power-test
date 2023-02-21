f16e0f89ea007326c044b15077b783e44eaa49d8
Cyprian <cyprianwaclaw@gmail.com>
2023-02-21 21:42:35 +0100

    test click

--- app.vue
+++ app.vue
@@ -2,10 +2,10 @@
   <!-- <Meta name="viewport" content="My app description" /> -->
   <Meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" />
   <div class="family black-color page-background">
-    <div class="click">
+    <!-- <div class="click"> -->
       <!-- <NuxtLoadingIndicator /> -->
       <NuxtPage />
-    </div>
+    <!-- </div> -->
   </div>
 </template>
 
